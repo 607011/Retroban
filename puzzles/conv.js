d2ad9bef11013c1b260c8c1610001c81f008be5d
Oliver Lau <oliver@ersatzworld.net>
2025-01-02 07:26:15 +0100

    Removed unused code.

--- puzzles/conv.js
+++ puzzles/conv.js
@@ -2,33 +2,6 @@
 
 const fs = require('node:fs');
 
-/*
-const axios = require('axios');
-async function downloadFrom(url) {
-    try {
-        const response = await axios.get(url);
-        return await response.data;
-    } catch (error) {
-        console.error(`Error downloading ${url}:`, error);
-        throw error;
-    }
-}
-
-async function download() {
-    const baseUrl = "https://ksokoban.online/levels/bunch3_";
-    for (let i = 0; i < 48; i++) {
-        console.info(`Processing ${baseUrl}${i}.js ...`);
-        const url = `${baseUrl}${i}.js`;
-        try {
-            const jsCode = await download(url);
-            fs.writeFileSync(`bunch-${i}.js`, jsCode);
-        } catch (error) {
-            console.error(`Failed to process ${url}:`, error);
-        }
-    }
-}
-*/
-
 let window = {};
 
 async function main() {
