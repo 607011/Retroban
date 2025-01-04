eac2dc6f85074564e06357022f1cabb612dfd674
Oliver Lau <oliver@ersatzworld.net>
2025-01-04 11:50:28 +0100

    Sources will be minified before deployment.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
 source .env
-rsync -Rrav index.html game.js images/* puzzles/xsb/* ${DST}
+
+DIST=dist
+
+mkdir -p ${DIST}
+mkdir -p ${DIST}/puzzles/xsb
+echo Copying files ...
+rsync -rav puzzles/xsb ${DIST}/puzzles
+cp -r index.html game.js images ${DIST}
+echo Minifying ...
+cd ${DIST}
+minify index.html -o index.html
+minify game.js -o game.js
+echo Optimizing images ...
+cd images
+optipng -quiet -o7 *.png
+cd ..
+echo Syncing to ${DST} ...
+rsync -Rrav . ${DST}
