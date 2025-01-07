3449c61ef0c548bb171fa620b4bd55ffcc6ec330
Oliver Lau <oliver@ersatzworld.net>
2025-01-07 18:19:43 +0100

    ffmpeg is running quietly.

--- _bin/convertsounds.sh
+++ _bin/convertsounds.sh
@@ -2,8 +2,9 @@
 
 mkdir -p sounds
 for wav in "completed" "step" "push"; do
-    ffmpeg -y -i _raw/${wav}.wav -vn -ar 44100 -ac 1 -b:a 96k sounds/${wav}.mp3
-    ffmpeg -y -i _raw/${wav}.wav -vn -ar 24000 -ac 1 -b:a 96k sounds/${wav}.wav
-    ffmpeg -y -i sounds/${wav}.wav -vn -ac 1 -acodec libopus sounds/${wav}.webm
-    ffmpeg -y -i sounds/${wav}.wav -vn -ac 1 -acodec libvorbis sounds/${wav}.ogg
+    echo Processing ${wav} ...
+    ffmpeg -y -hide_banner -loglevel error -i _raw/${wav}.wav -vn -ar 44100 -ac 1 -b:a 96k sounds/${wav}.mp3
+    ffmpeg -y -hide_banner -loglevel error -i _raw/${wav}.wav -vn -ar 24000 -ac 1 -b:a 96k sounds/${wav}.wav
+    ffmpeg -y -hide_banner -loglevel error -i sounds/${wav}.wav -vn -ac 1 -acodec libopus sounds/${wav}.webm
+    ffmpeg -y -hide_banner -loglevel error -i sounds/${wav}.wav -vn -ac 1 -acodec libvorbis sounds/${wav}.ogg
 done
