097b85d5e34c45debba0d2aa0e3c6e7cf423543c
Oliver Lau <oliver@ersatzworld.net>
2025-01-07 11:57:27 +0100

    Moved deployment script to _bin.

---
+++ _bin/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+source .env
+
+DIST=dist
+
+mkdir -p ${DIST}
+echo Copying files ...
+rsync -Rrav puzzles/xsb ${DIST}
+cp puzzles/list.txt ${DIST}/puzzles
+cp -r index.html js images manifest.json service-worker.js ${DIST}
+echo Minifying ...
+minify ${DIST}/index.html -o ${DIST}/index.html
+minify ${DIST}/js/game.js -o ${DIST}/js/game.js
+echo Optimizing images ...
+optipng -quiet -o7 ${DIST}/images/*.png
+echo Syncing to ${DST} ...
+cd ${DIST}
+rsync -Rrav . ${DST}
